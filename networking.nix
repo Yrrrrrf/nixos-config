1457a35aa867cbe90192a77014d55aac503ee333
Fernando Bryan Reza Campos <fer.rezac@outlook.com>
2025-08-11 19:46:29 -0600

    Reconfigures networking settings
    Replaces the old networking configuration with a simplified version.
    This change enables NetworkManager and OpenSSH, and sets the hostname.
    The previous configuration contained commented-out, specific Wi-Fi profiles, which are removed.

--- networking.nix
+++ networking.nix
@@ -1,64 +1,16 @@
 # /etc/nixos/networking.nix
 # Corrected version using the 'ensureProfiles' option for NetworkManager.
 
-{ config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  ...
+}:
 
 {
   networking.hostName = "G14";
 
-  # This remains correct. We are telling NixOS to use NetworkManager.
   networking.networkmanager.enable = true;
-
-  # This is the correct declarative method for adding connection profiles
-  # that NetworkManager will use.
-  #networking.networkmanager.ensureProfiles.profiles = {
-  #  # Each attribute here is a new connection profile.
-  #  # The name "TotalPlay_Sala_5Grapida_Profile" is arbitrary but should be unique.
-  #  "TotalPlay_Sala_5Grapida_Profile" = {
-  #    connection = {
-  #      id = "TP_S_5G";
-  #      type = "wifi";
-  #      autoconnect = true;
-  #    };
-  #    wifi = {
-  #      ssid = "TotalPlay_Sala_5Grapida";
-  #    };
-  #    "wifi-security" = {
-  #      key-mgmt = "wpa-psk";
-  #      psk = "madonna1";
-  #    };
-  #  };
-
-  #  "TotalPlay_Sala_2.4Grapida_Profilee" = {
-  #    connection = {
-  #      id = "TP_S_2.4G";
-  #      type = "wifi";
-  #      autoconnect = true;
-  #    };
-  #    wifi = {
-  #      ssid = "TotalPlay_Sala_2.4Gnormal";
-  #    };
-  #    "wifi-security" = {
-  #      key-mgmt = "wpa-psk";
-  #      psk = "madonna1";
-  #    };
-  #  };
-
-  #  "speed_Profile" = {
-  #    connection = {
-  #      id = "speed";
-  #      type = "wifi";
-  #      autoconnect = true;
-  #    };
-  #    wifi = {
-  #      ssid = "speed";
-  #    };
-  #    "wifi-security" = {
-  #      key-mgmt = "wpa-psk";
-  #      psk = "madonna1";
-  #    };
-  #  };
-  # };
 
   services.openssh.enable = true;
 }
